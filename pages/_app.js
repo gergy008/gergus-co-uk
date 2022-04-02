06120839b383e0949ac7ea76a40adbd5dca658c8
Gergy <gergy008@hotmail.co.uk>
2022-04-02 01:34:49 +0100

    Initial creation of the website
    Changes to allow a basic look and feel of the website. Better than the next.js start page!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider as NextThemeProvider} from 'next-themes'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+// 2. Call `createTheme` and pass your custom values
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+  }
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemeProvider>
+  )
 }
 
 export default MyApp
