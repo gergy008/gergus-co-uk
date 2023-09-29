8204326c6c2b754b21d10dbc7d793f34ad3ee649
Gergy <steven@gergus.co.uk>
2023-09-29 12:45:12 +0100

    Fix attempt Inv. prop. align found on tag footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div>
           &nbsp;
         </div>
-        <footer justify="center" align="center">
+        <footer justify="center" style="text-align:center;">
             <p>© 2022 Steven Gergus. <a href="https://github.com/gergy008/gergus-co-uk" target={"_blank"} rel="noreferrer">Source open and freely available under licence.</a></p>
         </footer>
     </>
